4b69bb902c7b7f1d343ba9dd1105cda488cd50ac
Elena Mironova <mironova_elenag@mail.ru>
2025-10-21 19:42:31 +0300

    Изменили логику метода

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -2,6 +2,7 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
+        System.out.println("---");
         System.out.println("Hello Word!");
         System.out.println("0000");
         System.out.println("2222");
@@ -10,8 +11,8 @@ public class MainApplication {
 
 
 public static void doSomething() {
+    System.out.println("0");
     System.out.println("1");
-    System.out.println("2");
-    System.out.println("3");
+    System.out.println("1");
 }
 }
